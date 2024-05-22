19094814e1626b62fe21b31cc0aa34566b41bc7c
notkoln <169077739+notkoln@users.noreply.github.com>
2024-05-22 12:40:38 -0400

    Update counter.js

--- counter.js
+++ counter.js
@@ -1,19 +1,21 @@
-var clicks = localStorage.getItem("clicks");
+var clicks = localStorage.getItem("taps");
 
-// checks if there is no value called clicks, if there is none, add one and set it to zero
+// checks if there is no value called taps, if there is none, add one and set it to zero
 if (!clicks) {
-  localStorage.setItem("clicks", "0");
+  localStorage.setItem("taps", "0");
 }
 
-// loads the amount of clicks the user has
-var clicksInt = parseInt(localStorage.getItem("clicks"))
-localStorage.setItem("clicks", clicksInt)
+// getting the button from the document
+var btn = document.getElementById("clicker");
+
+// loads the amount of taps the user has
+var clicksInt = parseInt(localStorage.getItem("taps"));
+localStorage.setItem("clicks", clicksInt);
 btn.innerText = `${clicksInt}`
 
-var btn = document.getElementById("clicker");
 btn.onclick = function() {
-  // increases the amount of clicks by one and saves it to local storage
-  var clicksInt = parseInt(localStorage.getItem("clicks"))
-  localStorage.setItem("clicks", ++clicksInt)
+  // increases the amount of taps by one and saves it to local storage
+  var clicksInt = parseInt(localStorage.getItem("taps"))
+  localStorage.setItem("taps", ++clicksInt)
   btn.innerText = `${clicksInt}`
 }
